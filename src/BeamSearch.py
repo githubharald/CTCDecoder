5e73bb9063e769e402eae0cd43c92ba171953245
Harald Scheidl <harald_scheidl@hotmail.com>
2018-07-03 19:14:33 +0200

    beam search: only normalize LM score but not the total probability

--- src/BeamSearch.py
+++ src/BeamSearch.py
@@ -9,106 +9,118 @@ class BeamEntry:
 		self.prTotal = 0 # blank and non-blank
 		self.prNonBlank = 0 # non-blank
 		self.prBlank = 0 # blank
-		self.y = () # labeling at current time-step
+		self.prText = 1 # LM score
+		self.lmApplied = False # flag if LM was already applied to this beam
+		self.labeling = () # beam-labeling
 
 
 class BeamState:
-	"information about beams at specific time-step"
+	"information about the beams at specific time-step"
 	def __init__(self):
 		self.entries = {}
 
 	def norm(self):
-		"length-normalise probabilities to avoid penalising long labelings"
+		"length-normalise LM score"
 		for (k, _) in self.entries.items():
-			labelingLen = len(self.entries[k].y)
-			self.entries[k].prTotal = self.entries[k].prTotal ** (1.0 / (labelingLen if labelingLen else 1))
+			labelingLen = len(self.entries[k].labeling)
+			self.entries[k].prText = self.entries[k].prText ** (1.0 / (labelingLen if labelingLen else 1.0))
 
 	def sort(self):
-		"return beams sorted by probability"
-		u = [v for (k, v) in self.entries.items()]
-		s = sorted(u, reverse=True, key=lambda x: x.prTotal)
-		return [x.y for x in s]
+		"return beam-labelings, sorted by probability"
+		beams = [v for (_, v) in self.entries.items()]
+		sortedBeams = sorted(beams, reverse=True, key=lambda x: x.prTotal*x.prText)
+		return [x.labeling for x in sortedBeams]
 
 
-def calcExtPr(k, y, t, mat, beamState, lm, classes):
-	"probability for extending labeling y to y+k"
-
-	# language model (char bigrams)
-	bigramProb = 1
-	if lm:
-		c1 = classes[y[-1] if y else classes.index(' ')]
-		c2 = classes[k]
-		lmFactor = 0.01 # controls influence of language model
-		bigramProb = lm.getCharBigram(c1, c2) ** lmFactor
-
-	# optical model (RNN)
-	if y and y[-1] == k:
-		return mat[t, k] * bigramProb*beamState.entries[y].prBlank
-	return mat[t, k] * bigramProb * beamState.entries[y].prTotal
+def applyLM(parentBeam, childBeam, classes, lm):
+	"calculate LM score of child beam by taking score from parent beam and bigram probability of last two chars"
+	if lm and not childBeam.lmApplied:
+		c1 = classes[parentBeam.labeling[-1] if parentBeam.labeling else classes.index(' ')] # first char
+		c2 = classes[childBeam.labeling[-1]] # second char
+		lmFactor = 0.01 # influence of language model
+		bigramProb = lm.getCharBigram(c1, c2) ** lmFactor # probability of seeing first and second char next to each other
+		childBeam.prText = parentBeam.prText * bigramProb # probability of char sequence
+		childBeam.lmApplied = True # only apply LM once per beam entry
 
 
-def addLabeling(beamState, y):
-	"adds labeling if it does not exist yet"
-	if y not in beamState.entries:
-		beamState.entries[y] = BeamEntry()
+def addBeam(beamState, labeling):
+	"add beam if it does not yet exist"
+	if labeling not in beamState.entries:
+		beamState.entries[labeling] = BeamEntry()
 
 
 def ctcBeamSearch(mat, classes, lm):
-	"beam search similar to algorithm described by Hwang - Character-Level Incremental Speech Recognition with Recurrent Neural Networks"
+	"beam search as described by the paper of Hwang et al. and the paper of Graves et al."
 
 	blankIdx = len(classes)
 	maxT, maxC = mat.shape
 	beamWidth = 25
 
-	# Initialise beam state
+	# initialise beam state
 	last = BeamState()
-	y = ()
-	last.entries[y] = BeamEntry()
-	last.entries[y].prBlank = 1
-	last.entries[y].prTotal = 1
+	labeling = ()
+	last.entries[labeling] = BeamEntry()
+	last.entries[labeling].prBlank = 1
+	last.entries[labeling].prTotal = 1
 
 	# go over all time-steps
 	for t in range(maxT):
 		curr = BeamState()
 
-		# get best labelings
-		BHat = last.sort()[0:beamWidth]
+		# get beam-labelings of best beams
+		bestLabelings = last.sort()[0:beamWidth]
 
-		# go over best labelings
-		for y in BHat:
+		# go over best beams
+		for labeling in bestLabelings:
+
+			# probability of paths ending with a non-blank
 			prNonBlank = 0
-			# if nonempty labeling
-			if y:
-				# seq prob so far and prob of seeing last label again
-				prNonBlank = last.entries[y].prNonBlank * mat[t, y[-1]]
+			# in case of non-empty beam
+			if labeling:
+				# probability of paths with repeated last char at the end
+				prNonBlank = last.entries[labeling].prNonBlank * mat[t, labeling[-1]]
 
-			# calc probabilities
-			prBlank = (last.entries[y].prTotal) * mat[t, blankIdx]
+			# probability of paths ending with a blank
+			prBlank = (last.entries[labeling].prTotal) * mat[t, blankIdx]
 
-			# save result
-			addLabeling(curr, y)
-			curr.entries[y].y = y
-			curr.entries[y].prNonBlank += prNonBlank
-			curr.entries[y].prBlank += prBlank
-			curr.entries[y].prTotal += prBlank + prNonBlank
+			# add beam at current time-step if needed
+			addBeam(curr, labeling)
 
-			# extend current labeling
-			for k in range(maxC - 1):
-				newY = y + (k,)
-				prNonBlank = calcExtPr(k, y, t, mat, last, lm, classes)
+			# fill in data
+			curr.entries[labeling].labeling = labeling
+			curr.entries[labeling].prNonBlank += prNonBlank
+			curr.entries[labeling].prBlank += prBlank
+			curr.entries[labeling].prTotal += prBlank + prNonBlank
+			curr.entries[labeling].prText = last.entries[labeling].prText # beam-labeling not changed, therefore also LM score unchanged from
+			curr.entries[labeling].lmApplied = True # LM already applied at previous time-step for this beam-labeling
 
-				# save result
-				addLabeling(curr, newY)
-				curr.entries[newY].y = newY
-				curr.entries[newY].prNonBlank += prNonBlank
-				curr.entries[newY].prTotal += prNonBlank
+			# extend current beam-labeling
+			for c in range(maxC - 1):
+				# add new char to current beam-labeling
+				newLabeling = labeling + (c,)
+
+				# if new labeling contains duplicate char at the end, only consider paths ending with a blank
+				if labeling and labeling[-1] == c:
+					prNonBlank = mat[t, c] * last.entries[labeling].prBlank
+				else:
+					prNonBlank = mat[t, c] * last.entries[labeling].prTotal
+
+				# add beam at current time-step if needed
+				addBeam(curr, newLabeling)
+				
+				# fill in data
+				curr.entries[newLabeling].labeling = newLabeling
+				curr.entries[newLabeling].prNonBlank += prNonBlank
+				curr.entries[newLabeling].prTotal += prNonBlank
+				
+				# apply LM
+				applyLM(curr.entries[labeling], curr.entries[newLabeling], classes, lm)
 
 		# set new beam state
 		last = curr
 
-	# normalise probabilities according to labeling length if LM is used
-	if lm:
-		last.norm()
+	# normalise LM scores according to beam-labeling-length
+	last.norm()
 
 	 # sort by probability
 	bestLabeling = last.sort()[0] # get most probable labeling
